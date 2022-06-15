0667aff494d2a07aa6a01a527107219dc28c41f6
Sabbir Ahmed <sabbir.ahmed845@gmail.com>
2022-06-15 17:08:09 +0600

    Added functionality to send and receive message from clients in the server.py file.

--- server.py
+++ server.py
@@ -5,7 +5,39 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENER_LIMIT = 5
+active_clients = []
 
+# Function to keep listen for upcoming messages from a client
+def listen_for_messages(client, username):
+    while True:
+        message = client.recv(2048).decode('utf-8')
+
+        if message != '':
+            final_msg = username + '->' + message
+            send_message_to_all(final_msg)
+        else:
+            print(f"An empty message received from {username}.")
+
+# Function to send message to a single client
+def send_message_to_client(client, message):
+    client.sendall(message.encode())
+
+# Function to send any message to all currently connected client
+def send_message_to_all(message):
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+# Function to handle client
+def client_handler(client):
+    while True:
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+            break
+        else:
+            print('Client username is empty')
+    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
+    
 # Main function
 def main():
     # Create socket class instance
@@ -24,6 +56,8 @@ def main():
     while True:
         client, address = server.accept()
         print(f"Successfully connected to client {address[0]} {address[1]}")
+
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 
 if __name__ == "__main__":
