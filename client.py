d751ed0d1b5114c423ce407889ca4e5ba645517a
Sabbir Ahmed <sabbir.ahmed845@gmail.com>
2022-06-15 17:24:34 +0600

    Added sending and receive message functionality to client.py file.

--- client.py
+++ client.py
@@ -5,6 +5,39 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 
+def listen_for_messages_from_server(client):
+    while True:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            username = message.split("->")[0]
+            content = message.split("->")[1]
+            print(f"{username} -> {content}")
+        else:
+            print('Message received from clinet is empty.')
+
+def send_message_to_server(client):
+    while True:
+        message = input("Message: ")
+
+        if message != '':
+            client.sendall(message.encode())
+        else:
+            print("Empty message")
+            exit(0)
+
+def communicate_to_server(client):
+    username = input('Enter username: ')
+
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        print('Username cannot be empty.')
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client,)).start()
+    send_message_to_server(client)
+
+# Main function
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,6 +47,8 @@ def main():
         print("Successfully connected to server.")
     except:
         print(f"Unable to connect to server {HOST} {PORT}.")
+
+    communicate_to_server(client)
 
 if __name__ == "__main__":
     main()
